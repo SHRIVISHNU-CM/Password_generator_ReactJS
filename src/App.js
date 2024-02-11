6b925d73c50d919291fe849075eda80ec986151c
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-11 13:17:38 +0530

    logic for password genrator

--- src/App.js
+++ src/App.js
@@ -1,31 +1,86 @@
-import { useState } from "react";
+import { useState, useCallback,useEffect,useRef } from "react";
 
 function App() {
-  const[Length,setLength] =useState(0);
-  const[NumberAllowed,setNumber]  =useState(false);
-  const[password,setPassword] = useState("");
-  const[charpassword,Setchar] = useState(false)
+  const [Length, setLength] = useState(5);
+  const [NumberAllowed, setNumber] = useState(false);
+  const [password, setPassword] = useState("");
+  const [charpassword, Setchar] = useState(false);
+  const passwordRef = useRef(null)
+  const generate = useCallback(()=>{
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(NumberAllowed) {
+      str += "0123456789"
+    }
+    if(charpassword){
+      str += "!@#$%^&*()_+"
+    }
+
+    for (let index = 0; index < Length; index++) {
+      const cha = Math.floor(Math.random() *str.length+1)
+      pass += str.charAt(cha)
+    }
+    setPassword(pass)
+  },[Length,NumberAllowed,charpassword])
+  useEffect(()=>{
+    generate()
+  },[Length,NumberAllowed,charpassword])
+  const copyPassword = ()=>{
+    window.navigator.clipboard.writeText(password);
+
+  }
+
   return (
     <>
       <h3 className="text-3xl bg-blue-400 font-bold text-center h-min p-2 w-full text-white">
         Password Generator
       </h3>
       <div className="flex justify-center mt-4 gap-3">
-        <input type="text" 
-        readOnly
-        className="outline-none border border-cyan-400 p-2 w-[400px] rounded-lg shadow-lg"
-        placeholder="enter here..."
+        <input type="text"
+          readOnly
+          value={password}
+          className="outline-none border border-cyan-400 p-2 w-[400px] rounded-lg shadow-lg"
+          placeholder="enter here..."
+          ref={passwordRef}
+
         />
 
-        <button className="pt-2 pb-2 p-3 border border-cyan-600 rounded-lg text-white bg-teal-400">Copy</button>
+        <button 
+        onClick={copyPassword}
+        className="pt-2 pb-2 p-3 border border-cyan-600 rounded-lg text-white bg-teal-400">Copy</button>
 
       </div>
       <div>
         <input type="range"
-        min={5}
-        max={100}
+          value={Length}
+          onChange={(e) => setLength(e.target.value)}
+          min={5}
+          max={20}
         />
+        <sapn>{Length}</sapn>
+
       </div>
+      <div>
+        <input
+          defaultChecked={NumberAllowed}
+          type="checkbox"
+          onChange={() => {
+            setNumber((prev) => !prev)
+          }}
+        />
+        <sapn>Numbers</sapn>
+      </div>
+      <div>
+        <input
+          defaultChecked={charpassword}
+          type="checkbox"
+          onChange={() => {
+            Setchar((prev) => !prev)
+          }}
+        />
+        <sapn>characters</sapn>
+      </div>
+
     </>
   );
 }
